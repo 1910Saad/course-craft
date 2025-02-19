c45615778ba54473518693d254e39d92eedbdbca
1910Saad <khansaad191004@gmail.com>
2025-02-19 13:39:19 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Header from "@/app/_components/Header";
+import Hero from "@/app/_components/Hero";
+
+export default function Home() {
+  return (
+    <div>
+      {/* Header */}
+      <Header/>
+
+
+      {/* Hero */}
+      <Hero/>
+    </div>
+  );
+}
