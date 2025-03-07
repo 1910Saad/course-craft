5029d777ae8bf8fdeb89ee11f132fce3c2714914
Shreyas Nair <126570047+Shreyas20004@users.noreply.github.com>
2025-03-07 09:44:00 +0530

    Update page.jsx

--- app/create-course/[courseId]/page.jsx
+++ app/create-course/[courseId]/page.jsx
@@ -12,7 +12,7 @@ function CourseLayout({params}) {
     useEffect(()=>{
         console.log(params.courseId)
         params&&GetCourse()
-    },[params])
+    },[params,user])
     const GetCourse = async() =>{
         const result = await db.select().from(CourseList).where(and(eq(CourseList.courseId,params?.courseId),eq(CourseList.createdBy,user?.primaryEmailAddress?.emailAddress)))
         setCourse(result[0])
