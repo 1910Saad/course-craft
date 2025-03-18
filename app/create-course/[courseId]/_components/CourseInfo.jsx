2a654f02add3d7ba2478f47a62c1c1648a300e21
Zaid <zaidshaikh0024@gmail.com>
2025-03-18 17:15:23 +0530

    feat: integrate firebase and add upload course image feat

--- app/create-course/[courseId]/_components/CourseInfo.jsx
+++ app/create-course/[courseId]/_components/CourseInfo.jsx
@@ -1,24 +1,72 @@
-import React from 'react'
-import Image from 'next/image'
+import React, { useState } from "react";
+import Image from "next/image";
 import { HiOutlinePuzzle } from "react-icons/hi";
-import{Button } from "@/components/ui/button"
-import EditCourseBasicInfo from './EditCourseBasicInfo';
-export default function CourseInfo({course,refreshData}) {
+import { Button } from "@/components/ui/button";
+import EditCourseBasicInfo from "./EditCourseBasicInfo";
+import { getDownloadURL, ref } from "firebase/storage";
+import { storage } from "@/configs/firebaseConfig";
+import { eq } from "drizzle-orm";
+import { CourseList } from "@/configs/schema";
+export default function CourseInfo({ course, refreshData }) {
+  const [selectedFile, setSelectedFile] = useState();
+  const onFileSelected = async (event) => {
+    const file = event.target.file[0];
+    setSelectedFile(URL.createObjectURL(file));
+    const fileName = Date.now() + ".jpg";
+    const storageRef = ref(storage, "ai-course/" + fileName);
+    await uploadBytes(storageRef, file)
+      .then((snapshot) => {
+        console.log("Uploaded a blob:", snapshot.metadata.name);
+      })
+      .then((res) => {
+        getDownloadURL(storageRef).then(async (downloadUrl) => {
+          console.log(downloadUrl);
+          await db
+            .update(CourseList)
+            .set({
+              courseBannner: downloadUrl,
+            })
+            .where(eq(CourseList.id, course?.id));
+        });
+      });
+  };
+
   return (
     <div className="p-10 border rounded-xl shadow-sm mt-5 relative">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
         <div className="">
-            <h2 className="font-bold text-3xl">{course?.courseOutput?.CourseName} <EditCourseBasicInfo course={course} refreshData={()=>refreshData(true)}/></h2>
-            <p className="text-sm text-gray-400 mt-3">{course?.courseOutput?.Description}</p>
-            <h2 className="font-medium mt-2 flex gap-2 items-center text-primary"><HiOutlinePuzzle /> {course?.category}</h2>
-            <Button className="bg-primary text-white w-full mt-5">Start</Button>
+          <h2 className="font-bold text-3xl">
+            {course?.courseOutput?.CourseName}{" "}
+            <EditCourseBasicInfo
+              course={course}
+              refreshData={() => refreshData(true)}
+            />
+          </h2>
+          <p className="text-sm text-gray-400 mt-3">
+            {course?.courseOutput?.Description}
+          </p>
+          <h2 className="font-medium mt-2 flex gap-2 items-center text-primary">
+            <HiOutlinePuzzle /> {course?.category}
+          </h2>
+          <Button className="bg-primary text-white w-full mt-5">Start</Button>
         </div>
         <div>
-            <Image src={'/placeholder.jpeg'}  width={300} height={300} className=" w-full rounded-xl h-[250px] object-cover" />
+          <label htmlFor="upload-image">
+            <Image
+              src={selectedFile?selectedFile:"/placeholder.jpeg"}
+              width={300}
+              height={300}
+              className=" w-full rounded-xl h-[250px] object-cover cursor-pointer"
+            />
+            <input
+              type="file"
+              id="upload-image"
+              className="opacity-0"
+              onChange={onFileSelected}
+            />
+          </label>
         </div>
-        
       </div>
-      
     </div>
-  )
+  );
 }
--- configs/schema.jsx
+++ configs/schema.jsx
@@ -1,14 +1,16 @@
-import { json,varchar,serial,pgTable } from "drizzle-orm/pg-core";
+import { json, varchar, serial, pgTable, boolean } from "drizzle-orm/pg-core";
 
-export const CourseList = pgTable("courseList",{
-    id: serial('id').primaryKey(),
-    courseId:varchar('courseId').unique().notNull(),
-    name: varchar('name').notNull(),
-    category:varchar('category').notNull(),
-    level:varchar('level').notNull(),
-    includeVideo:varchar('includeVideo').notNull().default("Yes"),
-    courseOutput:json('courseOutput').notNull(),
-    createdBy:varchar('createdBy').notNull(),
-    userName:varchar('userName'),
-    userProfileImage:varchar('userProfileImage'),
+export const CourseList = pgTable("courseList", {
+  id: serial("id").primaryKey(),
+  courseId: varchar("courseId").unique().notNull(),
+  name: varchar("name").notNull(),
+  category: varchar("category").notNull(),
+  level: varchar("level").notNull(),
+  includeVideo: varchar("includeVideo").notNull().default("Yes"),
+  courseOutput: json("courseOutput").notNull(),
+  createdBy: varchar("createdBy").notNull(),
+  userName: varchar("userName"),
+  userProfileImage: varchar("userProfileImage"),
+  courseBanner: varchar("courseBanner").default("/placeholder.png"),
+  publish: boolean("public").default(false),
 });
